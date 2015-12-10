5a65553224f47cba8e9259c22076e594630b67f8
Payamdel <payam_delfani1980@yahoo.com>
2015-12-10 01:27:38 +0100

    Assignment 1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+#File is already stored in my working directory and the data is loaded into R using the following codes where stringsAsFactors=FALSE is added to avoid the conversion of vectors to factors:
+filenamne <- "household_power_consumption.txt"
+rawfile <- read.table(filenamne, header= TRUE, sep= ";", stringsAsFactors= FALSE)
+
+#Create a subset of the entire dataset including the dates 2007-02-01 and 2007-02-02 only.
+Date <- rawfile[,1]
+SubDate <- is.element(Date , strsplit(c("1/2/2007","2/2/2007")," "))
+SubDate <- rawfile[SubDate,]
+
+#Making a histplot for plot 1. 
+png (filename = "plot1.png", width = 480, height= 480)
+hist (as.numeric(SubDate$Global_active_power), main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,47 @@
+ ######Plot 4
+ #File is already stored in my working directory and the data is loaded into R using the following codes where stringsAsFactors=FALSE is added to avoid the conversion of vectors to factors:
+ filenamne <- "household_power_consumption.txt"
+rawfile <- read.table(filenamne, header= TRUE, sep= ";", stringsAsFactors= FALSE)
+
+#Create a subset of the entire dataset including the dates 2007-02-01 and 2007-02-02 only.
+Date <- rawfile[,1]
+SubDate <- is.element(Date , strsplit(c("1/2/2007","2/2/2007")," "))
+SubDate <- rawfile[SubDate,]
+
+## read in date/time info in format "%d/%m/%Y %H:%M:%S"
+dates <- SubDate$Date
+times <- SubDate$Time
+#Use the paste function to concatenate vectors after converting to character. The cbind function would not work here as it will generate instead of character.
+x <- paste(dates, times)
+
+# Since i do live in Sweden and i want to express the dates in English, i do use the following code first.
+Sys.setlocale("LC_TIME", locale="USA")
+
+ # To convert the Date and Time variables to Date/Time classes in R using the strptime(), i.e., Date-time conversion from character vector.
+Date_Time <- strptime(x, format = "%d/%m/%Y %H:%M:%S")
+
+ #Making a plot
+png(filename = "plot4.png", width= 400, height= 400) 
+par(mfrow= c(2,2))
+plot (Date_Time,  as.numeric(SubDate$Global_active_power), type ="l", ylab = "Global Active Power (kilowatts)" , xlab="")
+plot (Date_Time, as.numeric(SubDate$Voltage), type ="l", col ="black", xlab= "datetime",  ylab = "Voltage" ) 
+plot (Date_Time, as.numeric(SubDate$Sub_metering_1), type ="l", col ="black", xlab= " ",  ylab = "Energy Sub metering" ) 
+lines (Date_Time, as.numeric(SubDate$Sub_metering_2), col= "red")
+lines (Date_Time, as.numeric(SubDate$Sub_metering_3), col= "blue")
+legend ("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty=1, lwd=1, bty = "n",  cex = 0.8)
+plot (Date_Time, as.numeric(SubDate$Global_reactive_power), type ="l", col ="black", xlab= "datetime",  ylab = "Global Reactive Power (kilowatts)" ) 
+dev.off()
+
+ 
+
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
